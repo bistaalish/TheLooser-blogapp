87a85ca5e2d708a289b277886e4be9a0de82bcde
Alish Bista <bistace321@gmail.com>
2023-10-03 06:23:56 +0545

    Feat: Blog site complete

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import './index.css';
 import './App.css';
  
 import {
- createBrowserRouter,
- RouterProvider,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import BlogsListPage from './pages/BlogsListPage';
- 
+import BlogPage from './pages/BlogPage'; 
+import AuthorPage from './pages/AuthorPage';
+
 const router = createBrowserRouter([
- {
-  path: "/",
-  element: <BlogsListPage /> ,
- },
-  
+  {
+    path: "/",
+    element: <BlogsListPage />,
+  },{
+    path: "/blog",
+    element: <BlogPage />,
+  }, {
+    path: "/about",
+    element: <AuthorPage />,
+  }, 
+
 ]);
  
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <RouterProvider router={router} />
+  <RouterProvider router={router} />
 );
